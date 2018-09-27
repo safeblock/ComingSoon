437990a91a7fa3c41dceed615fa7149c8cb8a993
Clint Mourlevat <clint.mourlevat@gmail.com>
2018-09-27 13:45:19 +0200

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComingSoon.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
